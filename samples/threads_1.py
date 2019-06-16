249398c79c0202813298bb43a671b4ea0eeb8926
Igor (python3) <ivm@Igors-MacBook-Pro.local>
2019-06-16 10:45:35 -0500

    tested with Python3

--- samples/threads_1.py
+++ samples/threads_1.py
@@ -3,22 +3,19 @@ from webpie import WebPieApp, WebPieHandler, run_server, Response, app_synchroni
 class MyApp(WebPieApp):
     
     def __init__(self, root_class):
-        WebPieApp.__init__(root_class)
+        WebPieApp.__init__(self, root_class)
         self.Memory = {}
     
 class Handler(WebPieHandler):
     
     @app_synchronized
-    def set_value(self, req, relpath, name=None, value=None, **args):
+    def set(self, req, relpath, name=None, value=None, **args):
         self.App.Memory[name]=value
         
     @app_synchronized
-    def get_value(self, req, relpath, name=None, **args):
+    def get(self, req, relpath, name=None, **args):
         return self.App.Memory.get(name, "(undefined)")
         
-    
-        
-        
-    
-application = MyApp(TopHandler)
+application = MyApp(Handler)
+application.run_server(8002)
 
