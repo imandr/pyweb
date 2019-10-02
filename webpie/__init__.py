a4ddac1e1d63eddcef3b710f389f5abe8ea4a675
Igor (python3) <ivm@Igors-MacBook-Pro.local>
2019-10-02 08:42:00 -0500

    Added WP* classes

--- webpie/__init__.py
+++ webpie/__init__.py
@@ -1,6 +1,7 @@
 from .WebPieApp import (WebPieApp, WebPieHandler, Response, app_synchronized, webmethod, atomic,
     WebPieStaticHandler)
 from .WebPieSessionApp import (WebPieSessionApp,)
+from .WPApp import WPApp, WPHandler
 from .HTTPServer import (HTTPServer, HTTPSServer, run_server)
 
 
