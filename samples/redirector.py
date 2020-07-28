ee86d0230e5a1c102c2169d95d681292eb022c40
Igor V Mandrichenko <ivm@fnal.gov>
2020-07-28 10:14:50 -0500

    .

--- samples/redirector.py
+++ samples/redirector.py
@@ -6,7 +6,6 @@ class Redirector(WPHandler):
     
     def __call__(self, request, relpath, **args):
         new_url = self.App.URLHead + request.path_qs
-        print("redirecting to:", new_url)
         self.redirect(new_url)
 
 class RedirectorApp(WPApp):
@@ -14,6 +13,10 @@ class RedirectorApp(WPApp):
     def __init__(self, url_head, handler):
         WPApp.__init__(self, handler)
         self.URLHead = url_head
+
+if not sys.argv[1:]:
+    print ("Usage: python redirector.py <new URL head>")
+    sys.exit(2)
 
 url_head = sys.argv[1]
 RedirectorApp(url_head, Redirector).run_server(8080)
